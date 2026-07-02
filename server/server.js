139c2eb40f29350bd20ad44ce9463eb1bc4d5c4e
Anchal Rajput <anchalrajput1606@gmail.com>
2026-07-02 15:10:40 +0530

    env file created

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
     res.send("Welcome to Task Manager API 🚀");
